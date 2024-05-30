1d2fe382f6f3190780da60d183d1d3a6f0412679
iloveilomilo <2311080940@alumno.utpuebla.edu.mx>
2024-05-30 10:47:39 -0600

    mostrarencarro LISTO

--- conductapractica28052024.js
+++ conductapractica28052024.js
@@ -1,25 +1,31 @@
-var carro = [];
-var spTotal = document.getElementById("total");
-var listaCarro = document.getElementById("carrito");
+    var carro = [];
+    var spTotal = document.getElementById("total");
+    var listaCarro = document.getElementById("carrito");
 
-function agregarCarro1(){
-    var objProducto = {
-    id : 1,
-    nombre:"Producto1",
-    precio : 375,
-    };
-    carro.push(objProducto);
-    calcularTotal();
-    mostrarEnCarro();
-}
-function calcularTotal(){
-    var elTotal = 0;
-    for(var p of carro)
-    {
-        elTotal+= p.precio;
+    function agregarCarro1(){
+        var objProducto = {
+        id : 1,
+        nombre:"Producto1",
+        precio : 375,
+        };
+        carro.push(objProducto);
+        calcularTotal();
+        mostrarEnCarro();
     }
-    spTotal.textContent = elTotal;
-}
-function mostrarEnCarro(){
-
-}
+    function calcularTotal(){
+        var elTotal = 0;
+        for(var p of carro)
+        {
+            elTotal+= p.precio;
+        }
+        spTotal.textContent = elTotal;
+    }
+    function mostrarEnCarro(){
+    listaCarro.textContent="";
+    for (var ObjP of carro){
+        var nodoProductoenCarro = document.createElement("li");
+        nodoProductoenCarro.classList.add("list-group-item" , "text-right" , "mx-2");
+        nodoProductoenCarro.textContent = ObjP.nombre + " - $ " + ObjP.precio;
+        listaCarro.appendChild(nodoProductoenCarro);
+    }
+    }
